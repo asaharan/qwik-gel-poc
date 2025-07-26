7506bd7ecde55cef4b7f6b08bbe678eed84d4fdb
Amit Saharan <asaharan812@gmail.com>
2025-07-27 00:26:15 +0530

    write setup script

---
+++ initialize.sh
@@ -0,0 +1,6 @@
+gel init
+get migrate
+gel ui
+
+echo "Please go to gel ui and setup Email Password provider and configure it to redirect to http://localhost:5178/auth/callback"
+echo "Use bun to install dependencies"
